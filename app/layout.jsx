d09001a7f8735c6e737d3fb7eb77f403ab26c713
Gilad Weinberger <giladwe21@gmail.com>
2025-02-27 17:57:27 +0200

    added google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Rubik } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const rubik = Rubik({
   subsets: ["latin"],
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${rubik.className} `}>{children}</body>
+      <GoogleAnalytics gaId="G-F1DZ58FE02" />
     </html>
   );
 }
