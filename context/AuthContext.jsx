e66d47fc796d076f22c3231d29a2cce06d676d2c
Gilad Weinberger <giladwe21@gmail.com>
2025-04-10 20:39:48 +0300

    feat: add "use client" directive to AuthContext for client-side rendering

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { createContext, useContext, useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "@/lib/firebase";
