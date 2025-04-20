5a2c420919ce21b63fa556bc937b16eec48a776d
eficefi <efudo123@gmail.com>
2025-04-20 18:55:24 +0300

    feat: Update Hero component content and layout for improved clarity and engagement

--- elements/homepage/Hero.jsx
+++ elements/homepage/Hero.jsx
@@ -3,20 +3,27 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <div className="max-w-3xl mx-auto mt-8 text-center">
-      <h1 className="text-4xl mb-6">ברוכים הבאים לאתר שלנו</h1>
-      <p className="text-lg mb-6">כאן תוכלו למצוא מידע על כל החיילים</p>
+      <img src="favicon.png" alt="favicon" className="w-[500] mr-[125]"/>
+      <h1 className="text-4xl">ברוכים הבאים לאתר memory.il</h1>
+      <p className="text-lg mb-6">כאן תוכלו למצוא מידע על חיילים שנפלו</p>
       <Link
         href="/soldiers"
         className="bg-blue-600 px-4 py-2 rounded-lg hover:bg-blue-700"
       >
-        לכל החיילים
+        לעמוד כל החיילים
       </Link>
-      <h1 className="text-center text-[30px] mt-11">על הפרוייקט</h1>
+      <h1 className="text-center text-[30px] mt-40">על הפרוייקט</h1>
       <hr className="w-[50%] mt-1 mb-4 mx-auto" />
-      <p className="text-center text-[20px]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa ut officia suscipit earum exercitationem natus laudantium, non beatae libero modi reprehenderit nihil, numquam dignissimos doloremque nesciunt voluptatum alias et ad.</p>
+      <p className="text-center text-[20px]">  הפרויקט הזה נולד כדי להנציח את זכרם של חיילים שנפלו – לא רק בזיכרון, אלא גם בלב הנוף של הארץ שהם אהבו.<br /><br />
+  בכל מעיין או מסלול טבע ברחבי הארץ הוספנו ברקוד קטן. כשסורקים אותו, מגיעים לדף אישי שמספר את סיפורו של חייל – התחביבים, החלומות, אהבתו לטיולים, והחיים שקדמו לנפילה.<br /><br />
+  המטרה היא לחבר בין המקום לזיכרון, בין ההווה להיסטוריה, וליצור חוויה מרגשת, אינטראקטיבית, שמלווה את המטיילים תוך כדי הדרך. כל סריקה היא רגע של זיכרון, אבל גם תזכורת לערכים של אהבת הארץ, רוח, ואנושיות.<br /><br />
+  כי כל מסלול הוא הזדמנות לזכור – ולספר את הסיפור שלהם, מחדש.</p>
       <h1 className="text-center text-[30px] mt-8">קצת עלינו</h1>
       <hr className="w-[50%] mt-1 mb-4 mx-auto" />
-      <p className="text-center text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam error nihil labore? Error sunt officia qui tenetur accusantium eos, id eum iure, praesentium ipsam consequuntur ea inventore quia laboriosam sequi.</p>
+      <p className="text-center text-[20px]"><br /><br />
+  אנחנו שלושה חבר'ה שלומדים בישיבת תמר. הרעיון לפרויקט הזה נולד מתוך הרצון שלנו לעשות משהו עם משמעות – להנציח חיילים שנפלו, בצורה שתיגע באנשים ותתחבר למה שאנחנו אוהבים: לטייל, להרגיש את הארץ, ולהשתמש בטכנולוגיה כדי ליצור חיבורים.<br /><br />
+  את הפרויקט יצרנו יחד עם המרכז לחדשנות, שם עזרו לנו להפוך את הרעיון למציאות.<br /><br />
+  מבחינתנו זה הרבה מעבר לפרויקט – זו דרך לזכור, להרגיש, ולהזכיר לכולם שלא משנה איפה מטיילים, יש תמיד מישהו שצעד פה לפנינו.</p>
     </div>
   );
 };
